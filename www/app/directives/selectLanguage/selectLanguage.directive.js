f8654ed21d45a57d6c2532bbfb89ff0ea14694b7
Dmitry Prudnikov <dmitry.prudnikov@amag.ch>
2016-07-08 12:39:22 +0200

    add selectLanguage directive

---
+++ www/app/directives/selectLanguage/selectLanguage.directive.js
@@ -0,0 +1,35 @@
+;(function () {
+    'use strict';
+
+    var dependencies = [];
+
+    var app = angular.module('app.selectLanguage.directive', dependencies);
+
+    app.directive('selectLanguage', function () {
+        return {
+            restrict: 'E',
+            controller: 'SelectLanguageCtrl',
+            scope: {
+                ngModel: '='
+            },
+            templateUrl:'app/directives/selectLanguage/selectLanguage.tmpl.html',
+            link: function (scope, element, attrs, SelectLanguageCtrl) {
+                SelectLanguageCtrl.init(element, scope);
+            }
+        }
+    });
+
+    app.controller('SelectLanguageCtrl', ['$scope', function ($scope) {
+        var self = this;
+
+        this.init = function (element, scope) {
+            self.$element = element;
+            $scope.parent = scope;
+            $scope.$watch('selectedLanguage', function(newVal){
+                $scope.parent.ngModel = newVal;
+            });
+        };
+
+    }]);
+
+}());
--- www/app/views/login/login.ctrl.js
+++ www/app/views/login/login.ctrl.js
@@ -2,14 +2,16 @@
     'use strict';
 
     var dependencies = [
-        'app.selectBrand.directive'
+        'app.selectBrand.directive',
+        'app.selectLanguage.directive'
     ];
 
     var app = angular.module('app.login.ctrl',dependencies);
 
     app.controller('LoginCtrl', ['$scope',function($scope){
         $scope.user = {};
-        $scope.user.brand = 'BRAND_VW';
+        $scope.user.brand = undefined;
+        $scope.user.language = undefined;
         
     }]);
 
